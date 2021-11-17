7dd5c99fe9d82b6ccfb649cb48ecb7fd90cf3169
Nicolas Barragan Mena <nicolas.barragan@upb.edu.co>
2021-11-17 10:24:16 -0500

    Create Game Manager script

---
+++ Assets/Script/GameManager.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using Photon.Pun;
+using Photon.Realtime;
+
+public class GameManager : MonoBehaviourPunCallbacks
+{
+    #region Photon Callbacks
+
+    public override void OnLeftRoom()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    #endregion
+
+    #region Public Methods
+
+    public void LeaveRoom()
+    {
+        PhotonNetwork.LeaveRoom();
+    }
+
+    #endregion
+}
